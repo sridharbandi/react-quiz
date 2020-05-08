97ad07d01f5b0fa897c837d2d2afb0c3b855ccb0
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-05-08 22:58:37 +0200

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const header = () => (
+    <AppBar elevation={0} position='sticky' color='primary'>
+        <Toolbar>
+            <Typography variant="h5" color="inherit" className="Text">
+                Quiz
+            </Typography>
+        </Toolbar>
+    </AppBar>
+);
+
+export default header;
