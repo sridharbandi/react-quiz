5c9ffb4a80e4043bb5eb3b129952370ebb580c13
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-05-11 22:43:07 +0200

    Clean up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import React, { useReducer } from 'react';
 import Header from './components/Header';
-import Progress from './components/Progress';
 import Question from './components/Question';
 import QuizContext from './context/QuizContext';
 import Button from '@material-ui/core/Button';
@@ -12,8 +11,9 @@ import Typography from '@material-ui/core/Typography';
 import { green } from '@material-ui/core/colors';
 import CheckIcon from '@material-ui/icons/Check';
 import CloseIcon from '@material-ui/icons/Close';
-import Questions from './api/sampleData.json';
-
+import ReportProblemIcon from '@material-ui/icons/ReportProblem';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { useParams } from "react-router-dom";
 
 import {
   SET_ANSWERS,
@@ -25,39 +25,21 @@ import {
 } from './reducers/Const';
 import quizReducer from './reducers/QuizReducer';
 
+const quizData = require.context('./api', true, /\.(json)$/);
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#eb3b5a',
+      contrastText: '#fff'
+    }
+  }
+});
+
 function App() {
-  /* const Questions = [
-    {
-      id: 1,
-      question: 'Which statement about Hooks is not true?',
-      answers: [
-        { option: 'a', text: 'Hooks are 100% backwards-compatible and can be used side by side with classes' },
-        { option: 'b', text: 'Hooks are still in beta and not available yet' },
-        { option: 'c', text: "Hooks are completely opt-in, there's no need to rewrite existing code" },
-        { option: 'd', text: 'All of the above' }],
-      correct_answer: 'b',
-    },
-    {
-      id: 2,
-      question: 'Which one is not a Hook?',
-      answers: [
-        { option: 'a', text: 'useState()' },
-        { option: 'b', text: 'useConst()' },
-        { option: 'c', text: 'useReducer()' },
-        { option: 'd', text: 'All of the above' }],
-      correct_answer: 'b',
-    },
-    {
-      id: 3,
-      question: 'What Hook should be used for data fetching?',
-      answers: [
-        { option: 'a', text: 'useDataFetching()' },
-        { option: 'b', text: 'useApi()' },
-        { option: 'c', text: 'useEffect()' },
-        { option: 'd', text: 'useRequest()' }],
-      correct_answer: 'c',
-    },
-  ]; */
+  let { id } = useParams();
+  var jsonFileName = quizData.keys().filter(name => name === `./${id}.json`)[0];
+  const Questions = jsonFileName === undefined ? '' : quizData(jsonFileName);
 
   const initialState = {
     Questions,
@@ -137,60 +119,75 @@ function App() {
 
     dispatch({ type: SET_SHOW_RESULTS, showResults: true });
   };
-
-  if (showHomepage) {
+  if (Questions === '') {
     return (
-      <React.Fragment>
+      <ThemeProvider theme={theme}>
         <Header />
+        <ReportProblemIcon color="primary" className="Report-Problem" />
         <Typography variant="h3" color="inherit" align="center" className="Text-Color">
-          Hello, Welcome!<br></br>
+          Looks like something went wrong
+        </Typography>
+        <Typography variant="body1" color="primary" align="center">
+          Make sure your path param matches json file name
+        </Typography>
+      </ThemeProvider>
+    );
+  }
+  else if (showHomepage) {
+    return (
+      <ThemeProvider theme={theme}>
+        <React.Fragment>
+          <Header />
+          <Typography variant="h3" color="inherit" align="center" className="Text-Color">
+            Hello, Welcome!<br></br>
               Test your knowledge
-      </Typography>
-        <Grid container justify="center">
-          <Button variant="contained" color="primary" onClick={restart} size="large" disableElevation>
-            Start the Quiz!
+            </Typography>
+          <Grid container justify="center">
+            <Button variant="contained" color="primary" onClick={restart} size="large" disableElevation>
+              Start the Quiz!
               </Button>
-        </Grid>
+          </Grid>
 
-      </React.Fragment>
+        </React.Fragment>
+      </ThemeProvider>
     );
   }
   else if (showResults) {
     return (
-      <React.Fragment>
-        <Header />
-        <Typography variant="h3" color="inherit" align="center" className="Text-Color">
-          You got {((correctAnswers.length / answers.length) * 100).toFixed(1)}%<Typography variant="caption" color="inherit" align="center" className="Text-Color">({correctAnswers.length} of {answers.length})</Typography> of the questions correct!
-        </Typography>
-        <Typography variant="button" color="inherit" align="center" className="Text-Color">
-          Results
-        </Typography>
-        <ul>{renderResultsData()}</ul>
-        <Grid container justify="center">
-          <Button variant="contained" onClick={restart} color="primary" size="large" disableElevation>
-            Try Again?
-            </Button>
-        </Grid>
-      </React.Fragment>
-    );
-  } else {
-    return (
-      <QuizContext.Provider value={{ state, dispatch }}>
+      <ThemeProvider theme={theme}>
         <React.Fragment>
           <Header />
-          <Progress
-            total={Questions.length}
-            current={currentQuestion + 1}
-          />
-          <Question />
-          {renderError()}
+          <Typography variant="h3" color="inherit" align="center" className="Text-Color">
+            You got {((correctAnswers.length / answers.length) * 100).toFixed(1)}%<Typography variant="caption" color="inherit" align="center" className="Text-Color">({correctAnswers.length} of {answers.length})</Typography> of the questions correct!
+        </Typography>
+          <Typography variant="button" color="inherit" align="center" className="Text-Color">
+            Results
+        </Typography>
+          <ul>{renderResultsData()}</ul>
           <Grid container justify="center">
-            <Button variant="contained" onClick={next} color="primary" size="large" disableElevation>
-              Confirm and Continue
+            <Button variant="contained" onClick={restart} color="primary" size="large" disableElevation>
+              Try Again?
             </Button>
           </Grid>
         </React.Fragment>
-      </QuizContext.Provider>
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <ThemeProvider theme={theme}>
+        <QuizContext.Provider value={{ state, dispatch }}>
+          <React.Fragment>
+            <Header />
+            <Question />
+            {renderError()}
+            <Grid container justify="center">
+              <Button variant="contained" onClick={next} color="primary" size="large" disableElevation>
+                Confirm and Continue
+            </Button>
+            </Grid>
+          </React.Fragment>
+        </QuizContext.Provider>
+      </ThemeProvider>
     );
   }
 }
