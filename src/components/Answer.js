82302398bfe741b18578ff7a2951726d20a6c17b
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-05-11 22:43:49 +0200

    Clean up

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
 
 function Answer(props) {
 
     return (
-        <FormControlLabel value={props.index} control={<Radio color="primary"/>} label={props.answer} />
+        <Paper elevation={0} className="Paper">
+            <FormControlLabel className="Full-Width" value={props.index} control={<Radio color="primary" />} label={props.answer} />
+        </Paper>
     );
 }
 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react';
 import Answer from './Answer';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Progress from './Progress';
 import quizContext from '../context/QuizContext';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControl from '@material-ui/core/FormControl';
 import Typography from '@material-ui/core/Typography';
 import { SET_CURRENT_ANSWER, SET_ERROR } from '../reducers/Const';
 import ReactHtmlParser from 'react-html-parser';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 
 function Question() {
@@ -24,24 +26,37 @@ function Question() {
     };
 
     return (
-        <Card className="Card">
-            <CardContent>
-                <FormControl component="fieldset">
-                    <Typography variant="h4" color="inherit" align="center" className="Text-Color">
-                        {ReactHtmlParser(question.question)}
-                    </Typography>
-                    <RadioGroup aria-label="answer" name="answers" value={currentAnswer} onChange={handleChange}>
-                        {question.answers.map((a, i) => (
-                            <Answer
-                                key={i}
-                                index={a.option}
-                                answer={ReactHtmlParser(a.text)}
-                            />
-                        ))}
-                    </RadioGroup>
-                </FormControl>
-            </CardContent>
-        </Card>
+        <div className="Questions">
+            <Grid container justify="center" alignItems="center">
+                <Grid item xs={12} md={8} className="Grid-Item">
+                    <Progress
+                        total={Questions.length}
+                        current={currentQuestion + 1}
+                    />
+                    <FormControl component="fieldset" className="Full-Width">
+                        <Typography variant="h4" color="inherit" align="center" className="Text-Color">
+                            {question.id}. {ReactHtmlParser(question.question)}
+                        </Typography>
+                        {question.code &&
+                            <div className="Code-Block">
+                                <SyntaxHighlighter showLineNumbers language="javascript" style={darcula}>
+                                    {question.code.join('\n')}
+                                </SyntaxHighlighter>
+                            </div>
+                        }
+                        <RadioGroup aria-label="answer" name="answers" value={currentAnswer} onChange={handleChange}>
+                            {question.answers.map((a, i) => (
+                                <Answer
+                                    key={i}
+                                    index={a.option}
+                                    answer={ReactHtmlParser(a.text)}
+                                />
+                            ))}
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+            </Grid>
+        </div>
     );
 }
 
