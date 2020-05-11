3589f5900f94b2b537dfca9e5303194639f7dcf7
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-05-11 22:43:28 +0200

    Configured Router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/:id?" component={App} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
