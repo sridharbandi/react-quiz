d77649d883edc9d842b3184ea19381cce66fee19
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-05-23 16:12:15 +0200

    Updates

---
+++ precache-manifest.c99f2cb07b39ac90d6ee59d4b6679a8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd33e1255bdc4b39b188aa788ed73ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2b899c8382a65507756",
+    "url": "/static/css/main.ee445235.chunk.css"
+  },
+  {
+    "revision": "fc1a4d1485da805fc40c",
+    "url": "/static/js/2.b058ace4.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/static/js/2.b058ace4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b899c8382a65507756",
+    "url": "/static/js/main.739a23ab.chunk.js"
+  },
+  {
+    "revision": "1b8a80a24ac7d72fdcc6",
+    "url": "/static/js/runtime-main.1cc782de.js"
+  },
+  {
+    "revision": "2c35d601b968c0e552413c6cbe224a12",
+    "url": "/static/media/Background.2c35d601.png"
+  }
+]);
